045c7a0ed438c44bdc61f32ff7563b464c0440fd
hasan-ghaforian <mohammadhasanghaforian@gmail.com>
2015-02-19 09:01:12 +0330

    Pt

--- PDF/src/ExtractText.java
+++ PDF/src/ExtractText.java
@@ -8,7 +8,7 @@ import org.apache.pdfbox.util.PDFTextStripper;
 
 public class ExtractText {
 	private final int it = 0;
-	private final int i = 1008;
+	private final int i = 1009;
 	
 	public static void main(String[] args) {
 		PDDocument pd;
